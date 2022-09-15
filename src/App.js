93ab7fa452a9e7d7a9cf3ba12046e5a391e0c4b3
Carlos Diones <carlosdiones28@gmail.com>
2022-09-14 22:55:04 -0300

    cria projeto

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header';
+
+import './App.css';
+
+function App() {
+  return (
+   <Header></Header>
+  );
+}
+
+export default App;
