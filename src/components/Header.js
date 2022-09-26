6dbec4ecb4847b57f1ddf958df2008121a5a7746
Carlos Diones <carlosdiones28@gmail.com>
2022-09-26 20:34:18 -0300

    removes Header.css

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,16 @@
-
-
 function Header()
 {
     return (
-        <></>
+        <>
+        <div>
+            <a href="#">XYZ Logo</a>
+            <a href="#">Início</a>
+            <a href="#">A XYZ</a>
+            <a href="#">Webmail</a>
+            <a href="#">Quem somos</a>
+            <a href="#">Contato</a>
+        </div>
+        </>
     );
 }
 
